9dc9c318755a3031289a2cc10668154769ca06ed
stephen <nwabuezesc@gmail.com>
2022-09-05 15:09:19 -0700

    setting up firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello from stephen')
+import { initializeApp } from 'firebase/app';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: "learn-firebase-5e508.firebaseapp.com",
+    projectId: "learn-firebase-5e508",
+    storageBucket: "learn-firebase-5e508.appspot.com",
+    messagingSenderId: "493433309881",
+    appId: "1:493433309881:web:083760a44213a0b3a16968"
+};
+
+initializeApp(firebaseConfig);
