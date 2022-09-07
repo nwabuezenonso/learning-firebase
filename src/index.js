9911d8b29efd20cb8d844e6302d82f8836e59732
stephen <nwabuezesc@gmail.com>
2022-09-07 11:04:51 -0700

    firestore setup and fetching data

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs} from 'firebase/firestore'; 
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -14,3 +15,26 @@ const firebaseConfig = {
 };
 
 initializeApp(firebaseConfig);
+
+
+// init service by invoking the function
+const db = getFirestore();
+
+
+// collection ref ( get a reference to a specific collection)
+const colRef = collection(db, 'Books');
+
+// get collection data
+
+getDocs(colRef)   //the data returns a promise(performs a function once data is complete or not)
+    .then((snapshot) => {    // function that contain a snapshot of our documents
+        let books = [];
+        snapshot.docs.forEach((doc) => {
+            books.push({...doc.data(), id: doc.id})  // spread allow us expand an array and take a copy of an existing array
+        })
+
+        console.log(books)
+    })
+    .catch(err => {
+        console.log(err.message)
+    })
